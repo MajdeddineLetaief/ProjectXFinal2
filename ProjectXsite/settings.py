a1646a7577cb49d31806156d472f43036ed83109
MajdeddineLetaief <majdeddine.letaief@gmail.com>
2018-02-21 16:21:54 +0100

    new settings jenkins update

--- ProjectXsite/settings.py
+++ ProjectXsite/settings.py
@@ -42,11 +42,9 @@ INSTALLED_APPS = [
 ]
 
 JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount'
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',   
 )
 
 MIDDLEWARE = [
